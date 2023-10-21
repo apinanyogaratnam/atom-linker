e76b64fd2dec4dc69b1135e96bf7a1bf68f493d2
apinanyogaratnam <apinan@ontopical.com>
2023-10-21 17:13:20 -0400

    fix: execute query method

--- execute_query.py
+++ execute_query.py
@@ -2,14 +2,15 @@ from database import Database
 from errors import InvalidQueryException
 
 
+# TODO: rename this class to something more descriptive
+# some ideas: QueryExecutor, QueryHandler, QueryProcessor
+# but this also stores the databases, so maybe something like
+# DatabaseManager or DatabaseHandler
 class ExecuteQuery:
-    def __init__(self, query: str) -> None:
-        self.query = query
+    def __init__(self) -> None:
         self.databases = {}
 
-    def execute_query(self) -> None:
-        query = self.query
-
+    def execute_query(self, query) -> None:
         if query.startswith("CREATE DATABASE"):
             database_name = query.split(" ")[2]
 
