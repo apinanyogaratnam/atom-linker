81d8db5b0f1354ff6d24062094a990f4926f621c
apinanyogaratnam <apinan@ontopical.com>
2023-10-21 20:42:34 -0400

    feat: data compression

---
+++ experimental/data_compression.py
@@ -0,0 +1,24 @@
+import lzma
+
+def compress_string(input_string):
+    compressed_data = lzma.compress(input_string.encode())
+    return compressed_data
+
+
+def decompress_string(compressed_data):
+    decompressed_data = lzma.decompress(compressed_data)
+    return decompressed_data.decode()
+
+
+# Example usage
+if __name__ == "__main__":
+    original_string = "This is an example string to be compressed using LZMA. LZMA provides a high compression ratio!"
+
+    # Compress the string
+    compressed_data = compress_string(original_string)
+    print("Original size:", len(original_string))
+    print("Compressed size:", len(compressed_data))
+
+    # Decompress the string
+    decompressed_string = decompress_string(compressed_data)
+    print("Decompressed string:", decompressed_string)
